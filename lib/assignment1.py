4e38879ddb8960af5d5a58979115249b6bb74e4d
Timo Nicolai <timonicolai@arcor.de>
2019-04-04 12:30:52 +0200

    Move common functions into module

---
+++ lib/assignment1.py
@@ -0,0 +1,70 @@
+from networks import SingleLayerFullyConnected, TrainHistory
+
+import matplotlib.pyplot as plt
+
+
+def _axes(rows, cols, figsize=None):
+    if figsize is None:
+        figsize = (5 * cols, 5 * rows)
+
+    _, axes = plt.subplots(rows, cols, figsize=figsize)
+
+    return axes
+
+
+def train_networks(ds_train, ds_val, hyperparams, pickle_dir):
+    loss = hyperparams.get('loss', 'cross_entropy')
+
+    for alpha, eta in zip(hyperparams['alpha'], hyperparams['eta']):
+        network = SingleLayerFullyConnected(
+    	    ds_train.input_size, ds_train.num_classes, alpha=alpha, loss=loss)
+
+        history = network.train(ds_train,
+    	                        ds_val,
+    	                        n_epochs=hyperparams['epochs'],
+    	                        verbose=True)
+
+        history.save(pickle_dir, [('alpha', alpha), ('eta', eta)])
+
+
+def load_histories(hyperparams, pickle_dir):
+    histories = []
+
+    for alpha, eta in zip(hyperparams['alpha'], hyperparams['eta']):
+        history = TrainHistory.load(
+            pickle_dir, [('alpha', alpha), ('eta', eta)])
+
+        history.add_title(r"$\lambda = {}$, $\eta = {}$".format(alpha, eta))
+
+        histories.append(history)
+
+    return histories
+
+
+def visualize_learning_curves(histories):
+    axes = _axes(len(histories), 2)
+
+    for i, history in enumerate(histories):
+        history.visualize(axes=axes[i, :])
+
+
+def visualize_performance(histories, ds):
+    axes = _axes(len(histories) // 2, 2)
+
+    for history, ax in zip(histories, axes.flatten()):
+        history.final_network.visualize_performance(
+           ds, ax=ax, title=history.title)
+
+
+def visualize_weights(histories, ds):
+    axes = _axes(len(histories),
+                 ds.num_classes,
+                 figsize=(ds.num_classes, len(histories)))
+
+    for i, history in enumerate(histories):
+        history.final_network.visualize_weights(axes=axes[i, :])
+
+        axes[i, 0].set_ylabel(history.title, labelpad=50, rotation=0)
+
+    for ax, label in zip(axes[-1, :], ds.labels):
+        ax.set_xlabel(label, rotation=45)
