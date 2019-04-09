6b937f5988e7e86a56cd2e589504127728538d34
Timo Nicolai <timonicolai@arcor.de>
2019-04-09 20:54:25 +0200

    Implement Recurrent.synthesize

--- lib/recurrent.py
+++ lib/recurrent.py
@@ -11,6 +11,8 @@ class Recurrent(Network):
 
         super().__init__(random_seed)
 
+        self.input_size = input_size
+
         self.b = self._rand_param((hidden_state_size, 1))
         self.c = self._rand_param((input_size, 1))
         self.U = self._rand_param((hidden_state_size, input_size))
@@ -28,6 +30,26 @@ class Recurrent(Network):
 
     def cost(self, ds, return_loss=False):
         raise ValueError('TODO')
+
+    def synthesize(self, x_init, h_init, length):
+        Y = np.empty((self.input_size, length))
+
+        h = h_init
+        x = x_init
+
+        for i in range(length):
+            a = self.W @ h + self.U @ x + self.b
+            h = np.tanh(a)
+            o = self.V @ h + self.c
+
+            p = np.exp(o)
+            p /= p.sum(axis=0)
+
+            x = np.random.multinomial(1, np.squeeze(p))[:, np.newaxis]
+
+            Y[:, i] = np.squeeze(x)
+
+        return Y
 
     def _gradients(self, ds):
         raise ValueError('TODO')
